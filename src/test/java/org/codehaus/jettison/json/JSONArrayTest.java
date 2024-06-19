23492a931743cef82a97c49466aca04c65130b77
Ievgenii Onyshchenko <eugene.onischenko@gmail.com>
2024-06-19 17:32:45 +0300

    Update src/test/java/org/codehaus/jettison/json/JSONArrayTest.java

--- src/test/java/org/codehaus/jettison/json/JSONArrayTest.java
+++ src/test/java/org/codehaus/jettison/json/JSONArrayTest.java
@@ -1,52 +1,58 @@
 package org.codehaus.jettison.json;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class JSONArrayTest extends TestCase {
+public class JSONArrayTest {
+
+    @Test
     public void testInvalidArraySequence() throws Exception {
-    	try {
-    	    new JSONArray("[32,");
-    	    fail("Exception expected");
-    	} catch (JSONException ex) {
-    		assertTrue(ex.getMessage().startsWith("JSONArray text has a trailing ','"));
-    	}
+        try {
+            new JSONArray("[32,");
+            fail("Exception expected");
+        } catch (JSONException ex) {
+            assertTrue(ex.getMessage().startsWith("JSONArray text has a trailing ','"));
+        }
     }
     
+    @Test
     public void testInvalidArraySequence2() throws Exception {
-    	try {
-    	    new JSONArray("[32,34");
-    	    fail("Exception expected");
-    	} catch (JSONException ex) {
-    		assertTrue(ex.getMessage().startsWith("Expected a ',' or ']'"));
-    	}
+        try {
+            new JSONArray("[32,34");
+            fail("Exception expected");
+        } catch (JSONException ex) {
+            assertTrue(ex.getMessage().startsWith("Expected a ',' or ']'"));
+        }
     }
     
+    @Test
     public void testEscapingInArrayIsOnByDefault() {
-      JSONArray array = new JSONArray();
-      array.put("a string with / character");
-      String expectedValue = "[\"a string with \\/ character\"]";
-      assertEquals(expectedValue, array.toString());
+        JSONArray array = new JSONArray();
+        array.put("a string with / character");
+        String expectedValue = "[\"a string with \\/ character\"]";
+        assertEquals(expectedValue, array.toString());
     }
     
+    @Test
     public void testEscapingInArrayIsTrunedOff() throws JSONException {
-   
-      JSONObject obj = new JSONObject();
-      obj.put("key", "http://example.com/foo");
-      obj.setEscapeForwardSlashAlways(false);
+        JSONObject obj = new JSONObject();
+        obj.put("key", "http://example.com/foo");
+        obj.setEscapeForwardSlashAlways(false);
 
-      JSONArray array = new JSONArray();
-      array.put("a string with / character");
-      array.put(obj);
-      array.setEscapeForwardSlashAlways(false);
+        JSONArray array = new JSONArray();
+        array.put("a string with / character");
+        array.put(obj);
+        array.setEscapeForwardSlashAlways(false);
       
-      System.out.println(array.toString());
-      String expectedValue = "[\"a string with / character\",{\"key\":\"http://example.com/foo\"}]";
-      assertEquals(expectedValue, array.toString());
+        System.out.println(array.toString());
+        String expectedValue = "[\"a string with / character\",{\"key\":\"http://example.com/foo\"}]";
+        assertEquals(expectedValue, array.toString());
     }
 
+    @Test
     public void testInfiniteLoop() {
         String str = "[*/*A25] **";
         try {
@@ -57,6 +63,7 @@ public class JSONArrayTest extends TestCase {
         }
     }
 
+    @Test
     public void testInfiniteLoop2() {
         String str = "[/";
         try {
@@ -67,13 +74,14 @@ public class JSONArrayTest extends TestCase {
         }
     }
 
+    @Test
     public void testIssue52() throws JSONException {
         JSONObject.setGlobalRecursionDepthLimit(10);
         new JSONArray("[{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {a:10}]");
         JSONObject.setGlobalRecursionDepthLimit(500);
     }
 
-    // https://github.com/jettison-json/jettison/issues/60
+    @Test
     public void testIssue60() throws JSONException {
         List<Object> list = new ArrayList<>();
         list.add(list);
@@ -83,5 +91,4 @@ public class JSONArrayTest extends TestCase {
             assertEquals(ex.getMessage(), "JSONArray has reached recursion depth limit of 500");
         }
     }
-
 }
